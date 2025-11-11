0d77ee1ad55c653041977f1d0a1e688979a7aba5
Võ Đăng Phát <vodangphat2002@gmail.com>
2025-11-11 20:37:08 +0700

    update

--- Client/my-app/src/Services/orderService.tsx
+++ Client/my-app/src/Services/orderService.tsx
@@ -1,11 +1,12 @@
 const API_BASE = "http://localhost:5292/api/Orders";
 
+const storedRole = localStorage.getItem("role");
+const roleId: string = storedRole ? JSON.parse(storedRole) : "";
+
 export const orderService = {
   async getAll(pageNumber = 1, pageSize = 10) {
-    const res = await fetch(`${API_BASE}/GetAll?type=-1&pageNumber=${pageNumber}&pageSize=${pageSize}`, {
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem("access_token")}`,
-      },
+    const res = await fetch(`${API_BASE}/GetAll?type=-1&pageNumber=${pageNumber}&pageSize=${pageSize}&roleId=${roleId}`, {
+      method: "GET",
     });
 
     if (!res.ok) throw new Error("Không thể tải danh sách đơn hàng");
@@ -13,6 +14,7 @@ export const orderService = {
     const text = await res.text();
     return text ? JSON.parse(text) : [];
   },
+
 
   async getById(id: string) {
     const res = await fetch(`${API_BASE}/GetById?id=${id}`, {
